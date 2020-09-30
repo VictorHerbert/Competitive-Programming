831e6705d422b8020e850188e0d49bec46e7126d
Victor Herbert <victorherberts@outlook.com>
2020-09-30 01:37:37 -0300

    remove trainning

--- Solutions/URI/2030.cpp
+++ Solutions/URI/2030.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 
-#if __has_include("print.h")
-#include "print.h"
-#define debug(x) print(#x, ":", x)
+#if __has_include("../Util/print.h")
+#include "../Util/print.h"
 #endif
 
 using namespace std;
 
-double EPS = 1e-9;
-int INF = INT_MAX/4;
-unsigned long long MOD = 1e9+7;
-#define TAM 1e6
+const double EPS = 1e-9;
+const int INF = INT_MAX / 4;
+const unsigned long long MOD = 1e9 + 7;
 
 #define ll long long
 #define vi vector<int>
@@ -18,101 +16,134 @@ unsigned long long MOD = 1e9+7;
 #define ull unsigned long long
 #define vll vector<ll>
 #define vvll vector<vector<ll>>
+#define vs vector<string>
 
-//#define mp make_pair
+#define mp make_pair
 
 #define pb push_back
 #define sz size
 
-#define window 10
+#define all(x) x.begin(), x.end()
 
-class Vertex{
-public:
-    int node;
-    int weight;
-};
+#define Edge pair<int, pair<int, bool>>
+#define make_edge(a, w, d) mp(a, mp(w, d))
+#define weight second.first
+#define dst first
+#define down second.second
 
-ostream &operator<<(ostream &os, const Vertex &v)
+vector<vector<Edge>> adjs;
+vector<bool> marc;
+ll acum = 0;
+
+int bfs(int vertex)
 {
-    os << "<n: " << v.node << ", w: " << v.weight << ">";
-    return os;
+    queue<int> q;
+    q.push(vertex);
+    marc[vertex] = true;
+
+    while (!q.empty())
+    {
+        int current = q.front();
+        q.pop();
+
+        debug(current);
+
+        for (Edge edge : adjs[current])
+        {
+            int dst = edge.dst;
+            if (!marc[dst])
+            {
+                q.push(dst);
+                debug(edge);
+                acum += edge.weight;
+                marc[dst] = true;
+            }
+        }
+    }
+
+    return acum;
 }
-
-Vertex mp(int a, int b){
-    Vertex v;
-    v.node = a;
-    v.weight = b;
-    return v;
-}
-
-
 
 int main()
 {
-    ios_base::sync_with_stdio(false); cin.tie(0);
-    //cout << setprecision(10) << fixed;
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(0);
 
-    int n=0,a=0,b=0,k=0,acum=0,g_acum = 0,pos=0;
+    int n = 0, a = 0, b = 0, k = 0, g_acum = 0, pos = 0;
     string s;
-    int prev,current;
+    int prev, current;
     char c;
-    map < string, set<string> > sdirs;
-    
-    map < string, int > table;
+    map<string, set<string>> sdirs;
+
+    map<string, int> table;
     table[""] = 0;
-    
 
     cin >> n;
-    while(n--){
+    while (n--)
+    {
         cin >> s;
-        s+='/';
+        s += '/';
 
         pos = 1;
 
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '/'){
-                string parent = s.substr(0,pos-1);
-                string child = s.substr(0,i);
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '/')
+            {
+                string parent = s.substr(0, pos - 1);
+                string child = s.substr(0, i);
 
                 if (table.count(child) == 0)
-                    table[child] = table.size()-1;                    
-                
+                    table[child] = table.size() - 1;
 
-                if (sdirs.count(parent) == 0){
+                if (sdirs.count(parent) == 0)
+                {
                     set<string> st;
                     st.insert(child);
                     sdirs[parent] = st;
                 }
                 else
-                    sdirs[parent].insert(child);                
+                    sdirs[parent].insert(child);
 
-                pos = i+1;
+                pos = i + 1;
             }
         }
     }
 
-
     // String adj list to int adj list
-    vector<vector<Vertex>> adj(table.size(), vector<Vertex>());
-    for (auto it : sdirs){
-        vector < Vertex > v;
-        for(string ss : it.second)
-            v.pb(mp(table[ss], ss.size() - it.first.size() 
-                + (it.first == "" ? 0 : 1)
-            ));
-        adj[table[it.first]] = v;
+    adjs = vector<vector<Edge>>(table.size(), vector<Edge>());
+
+    for (auto it : sdirs)
+    {
+        vector<Edge> &v = adjs[table[it.first]];
+
+        for (string ss : it.second)
+        {
+            v.pb(make_edge(
+                table[ss],
+                ss.size() - it.first.size() + (it.first == "" ? 0 : 1),
+                true));
+            adjs[table[ss]].pb(make_edge(
+                table[it.first],
+                ss.size() - it.first.size() + (it.first == "" ? 0 : 1),
+                false));
+        }
+        //adj[table[it.first]] = v;
         //cout << it.second << "\n";
     }
 
     // - string map to int map
-    
+
     debug(table);
     debug(sdirs);
-    debug(adj);
+    debug(adjs);
+    acum = 0;
+    marc = vector<bool>(table.size(), false);
+    //bfs(0);
+    print(acum);
 
     return 0;
 }
-
 
 //g++ -std=c++11 s.cpp -o s.exe & s.exe < in.txt > out.txt
 //python s.py < in.txt > out.txt
